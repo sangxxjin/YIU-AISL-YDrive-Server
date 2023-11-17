718ec5132403421564b04364dbd82d87fd9e6696
callikys <callikys@naver.com>
2023-11-17 20:35:22 +0900

    /carpool/create 로직 작성 완료

--- carpool/src/main/java/yiu/aisl/carpool/service/CarpoolService.java
+++ carpool/src/main/java/yiu/aisl/carpool/service/CarpoolService.java
@@ -1,5 +1,6 @@
 package yiu.aisl.carpool.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -7,26 +8,34 @@ import org.springframework.stereotype.Service;
 import yiu.aisl.carpool.Dto.CarpoolRequest;
 import yiu.aisl.carpool.domain.Carpool;
 import yiu.aisl.carpool.repository.CarpoolRepository;
+import yiu.aisl.carpool.security.JwtProvider;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CarpoolService {
     private final CarpoolRepository carpoolRepository;
+    private final JwtProvider jwtProvider;
+    private final HttpServletRequest httpServletRequest;
 
     public boolean create(CarpoolRequest request) throws Exception {
         try {
-            Carpool carpool = Carpool.builder()
-                    .carpoolNum(request.getCarpoolNum())
-                    .start(request.getStart())
-                    .end(request.getEnd())
-                    .date(request.getDate())
-                    .checkNum(request.getCheckNum())
-                    .memberNum(request.getMemberNum())
-                    .email(request.getEmail())
-                    .createdAt(request.getCreatedAt())
-                    .build();
-            carpoolRepository.save(carpool);
+            String authHeader = httpServletRequest.getHeader("Authorization");
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = authHeader.substring(7);
+                String email = jwtProvider.getEmail(token);
+                Carpool carpool = Carpool.builder()
+                        .carpoolNum(request.getCarpoolNum())
+                        .start(request.getStart())
+                        .end(request.getEnd())
+                        .date(request.getDate())
+                        .checkNum(request.getCheckNum())
+                        .memberNum(request.getMemberNum())
+                        .email(email)
+                        .createdAt(request.getCreatedAt())
+                        .build();
+                carpoolRepository.save(carpool);
+            }
         } catch (DataIntegrityViolationException e) {
             System.out.println(e.getMessage());
             throw  new Exception("잘못된 요청입니다.");
