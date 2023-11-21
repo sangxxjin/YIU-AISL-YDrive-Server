d24ee32b013f2d05840219ffaff3a2b3ca80a17a
sangxxjin <sj_0368@naver.com>
2023-11-21 14:33:29 +0900

    feat: 게시글 update할때 이메일 기반으로 작성자인지 확인하기 위한 email찾기

--- carpool/src/main/java/yiu/aisl/carpool/repository/CarpoolRepository.java
+++ carpool/src/main/java/yiu/aisl/carpool/repository/CarpoolRepository.java
@@ -1,7 +1,10 @@
 package yiu.aisl.carpool.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import yiu.aisl.carpool.domain.Carpool;
+import yiu.aisl.carpool.domain.User;
 
 public interface CarpoolRepository extends JpaRepository<Carpool, String> {
+  Optional<Carpool> findByEmail(String email);
 }
