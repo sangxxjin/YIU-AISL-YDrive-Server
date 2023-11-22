1843358003a5634140fce4163efc90dd8253b654
sangxxjin <sj_0368@naver.com>
2023-11-22 11:52:28 +0900

    feat: carpool delete 로직 구현

--- carpool/src/main/java/yiu/aisl/carpool/service/CarpoolService.java
+++ carpool/src/main/java/yiu/aisl/carpool/service/CarpoolService.java
@@ -21,51 +21,65 @@ import java.util.Date;
 @Transactional
 @RequiredArgsConstructor
 public class CarpoolService {
-    private final CarpoolRepository carpoolRepository;
-    private final JwtProvider jwtProvider;
-    private final HttpServletRequest httpServletRequest;
-    private final UserRepository userRepository;
+  private final CarpoolRepository carpoolRepository;
+  private final JwtProvider jwtProvider;
+  private final HttpServletRequest httpServletRequest;
+  private final UserRepository userRepository;
 
-    public boolean create(CarpoolRequest request) throws Exception {
-        try {
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
-            Date date = new Date(System.currentTimeMillis());
-            String authHeader = httpServletRequest.getHeader("Authorization");
-            if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                String token = authHeader.substring(7);
-                String email = jwtProvider.getEmail(token);
-                Carpool carpool = Carpool.builder()
-                        .carpoolNum(request.getCarpoolNum())
-                        .start(request.getStart())
-                        .end(request.getEnd())
-                        .date(request.getDate())
-                        .checkNum(request.getCheckNum())
-                        .memberNum(request.getMemberNum())
-                        .email(email)
-                        .createdAt(date)
-                        .build();
-                carpoolRepository.save(carpool);
-            }
-        } catch (DataIntegrityViolationException e) {
-            System.out.println(e.getMessage());
-            throw  new Exception("잘못된 요청입니다.");
-        }
-        return true;
+  public boolean create(CarpoolRequest request) throws Exception {
+    try {
+      SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+      Date date = new Date(System.currentTimeMillis());
+      String authHeader = httpServletRequest.getHeader("Authorization");
+      if (authHeader != null && authHeader.startsWith("Bearer ")) {
+        String token = authHeader.substring(7);
+        String email = jwtProvider.getEmail(token);
+        Carpool carpool = Carpool.builder()
+            .carpoolNum(request.getCarpoolNum())
+            .start(request.getStart())
+            .end(request.getEnd())
+            .date(request.getDate())
+            .checkNum(request.getCheckNum())
+            .memberNum(request.getMemberNum())
+            .email(email)
+            .createdAt(date)
+            .build();
+        carpoolRepository.save(carpool);
+      }
+    } catch (DataIntegrityViolationException e) {
+      System.out.println(e.getMessage());
+      throw new Exception("잘못된 요청입니다.");
     }
-    public void update(CustomUserDetails userDetails, int carpoolNum, CarpoolDto carpoolDto) {
-        String email = userDetails.getUser().getEmail();
-        Optional<Carpool> carpoolOptional = carpoolRepository.findByCarpoolNumAndEmail(carpoolNum, email);
+    return true;
+  }
 
-        if (carpoolOptional.isPresent()) {
-            Carpool carpool = carpoolOptional.get();
-            // 업데이트 로직을 수행하고 저장
-            carpool.setStart(carpoolDto.getStart());
-            carpool.setEnd(carpoolDto.getEnd());
-            carpool.setDate(carpoolDto.getDate());
-            carpool.setMemberNum(carpoolDto.getMemberNum());
-            carpoolRepository.save(carpool);
-        } else {
-            throw new IllegalArgumentException("찾을수가 없습니다.");
-        }
+  public void update(CustomUserDetails userDetails, int carpoolNum, CarpoolDto carpoolDto) {
+    String email = userDetails.getUser().getEmail();
+    Optional<Carpool> carpoolOptional = carpoolRepository.findByCarpoolNumAndEmail(carpoolNum,
+        email);
+
+    if (carpoolOptional.isPresent()) {
+      Carpool carpool = carpoolOptional.get();
+      // 업데이트 로직을 수행하고 저장
+      carpool.setStart(carpoolDto.getStart());
+      carpool.setEnd(carpoolDto.getEnd());
+      carpool.setDate(carpoolDto.getDate());
+      carpool.setMemberNum(carpoolDto.getMemberNum());
+      carpoolRepository.save(carpool);
+    } else {
+      throw new IllegalArgumentException("찾을수가 없습니다.");
     }
+  }
+
+  public void delete(CustomUserDetails userDetails, int carpoolNum) {
+    String email = userDetails.getUser().getEmail();
+    Optional<Carpool> carpoolOptional = carpoolRepository.findByCarpoolNumAndEmail(carpoolNum,
+        email);
+
+    if (carpoolOptional.isPresent()) {
+      carpoolRepository.delete(carpoolOptional.get());
+    } else {
+      throw new IllegalArgumentException("찾을수가 없습니다.");
+    }
+  }
 }
