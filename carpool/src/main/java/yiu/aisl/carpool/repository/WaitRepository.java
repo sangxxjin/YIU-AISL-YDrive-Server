1ce63a22426371fdf5b9b069e32aea9e4fc4a280
sangxxjin <sj_0368@naver.com>
2023-11-28 10:59:04 +0900

    게시글 신청받은 내용을 수락, 거절 repository 구현

--- carpool/src/main/java/yiu/aisl/carpool/repository/WaitRepository.java
+++ carpool/src/main/java/yiu/aisl/carpool/repository/WaitRepository.java
@@ -1,7 +1,12 @@
 package yiu.aisl.carpool.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import yiu.aisl.carpool.domain.Carpool;
 import yiu.aisl.carpool.domain.Wait;
 
 public interface WaitRepository extends JpaRepository<Wait, String> {
+  Optional<Wait> findByOwnerAndCarpoolNum_CarpoolNumAndWaitNum(String owner, Integer carpoolNum,
+      Integer waitNum);
+
 }
