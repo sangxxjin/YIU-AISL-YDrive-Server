2dc83a48ae209d92fc2530fe200b6ba89129f941
sangxxjin <sj_0368@naver.com>
2023-11-28 10:58:41 +0900

    게시글 신청받은 내용을 수락, 거절 dto 구현

---
+++ carpool/src/main/java/yiu/aisl/carpool/Dto/WaitDto.java
@@ -0,0 +1,14 @@
+package yiu.aisl.carpool.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class WaitDto {
+  private int checkNum;
+}
