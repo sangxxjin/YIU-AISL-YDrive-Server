058c9e8fb37d1d04e4c9114b1150562636f69038
callikys <callikys@naver.com>
2023-12-04 17:36:10 +0900

    /main API 에서 CarpoolDto 보다 CarpoolResponse 가 더 효율적이어서 바꿈

--- carpool/src/main/java/yiu/aisl/carpool/service/ScreenService.java
+++ carpool/src/main/java/yiu/aisl/carpool/service/ScreenService.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import yiu.aisl.carpool.Dto.CarpoolDto;
+import yiu.aisl.carpool.Dto.CarpoolResponse;
 import yiu.aisl.carpool.repository.CarpoolRepository;
 
 import java.util.List;
@@ -14,9 +15,9 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ScreenService {
     private final CarpoolRepository carpoolRepository;
-    public List<CarpoolDto> getCarpool() {
+    public List<CarpoolResponse> getCarpool() {
         return carpoolRepository.findAll().stream()
-                .map(CarpoolDto::new)
+                .map(CarpoolResponse::new)
                 .collect(Collectors.toList());
     }
 }
