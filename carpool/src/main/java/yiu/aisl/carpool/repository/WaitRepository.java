3ca55e25f7a793aa702e398e65ea3711682d66d5
callikys <callikys@naver.com>
2023-12-08 18:46:09 +0900

    내가 탑승자였을 때 이용내역 오류 문제 해결

--- carpool/src/main/java/yiu/aisl/carpool/repository/WaitRepository.java
+++ carpool/src/main/java/yiu/aisl/carpool/repository/WaitRepository.java
@@ -2,6 +2,8 @@ package yiu.aisl.carpool.repository;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import yiu.aisl.carpool.domain.Carpool;
@@ -10,11 +12,10 @@ import yiu.aisl.carpool.domain.Wait;
 public interface WaitRepository extends JpaRepository<Wait, String> {
   Optional<Wait> findByCarpoolNum_CarpoolNumAndWaitNum(Integer carpoolNum, Integer waitNum);
 
-  Optional<Wait> findByOwnerAndCarpoolNum_CarpoolNumAndWaitNum(String owner, Integer carpoolNum,
-      Integer waitNum);
+  Optional<Wait> findByOwnerAndCarpoolNum_CarpoolNumAndWaitNum(String owner, Integer carpoolNum, Integer waitNum);
 
   boolean existsByCarpoolNumAndGuest(Carpool carpool, String email);
 
-  @Query("SELECT w FROM Wait w JOIN FETCH w.carpoolNum c WHERE w.checkNum = 3")
-  List<Wait> findByCheckNumAndGuestWithCarpool(String email);
+  @Query("SELECT w FROM Wait w JOIN FETCH w.carpoolNum c WHERE w.checkNum = 3 AND w.guest = :email")
+  List<Wait> findByCheckNumAndGuestWithCarpool(@Param("email") String email);
 }
